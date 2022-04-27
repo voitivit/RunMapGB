b9f68545fc9f5319f440ddd617dd3d3c915a45a0
voitivit <kurbanov_emil@icloud.com>
2022-04-27 11:58:12 +0300

    добавил GoogleMap

---
+++ RunMapGB/RunMapGB/MapController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RunMapGB
+//
+//  Created by emil kurbanov on 27.04.2022.
+//
+
+import UIKit
+
+class MapController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
