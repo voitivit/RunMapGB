b7663216d8479571736bd2c4b9c19466ad35826e
voitivit <kurbanov_emil@icloud.com>
2022-06-07 12:07:24 +0300

    lesson8

---
+++ RunMapGB/RunMapGB/Model/PhotoMarker.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoMarker.swift
+//  RunMapGB
+//
+//  Created by emil kurbanov on 07.06.2022.
+//
+
+import Foundation
---
+++ RunMapGB/RunMapGB/View/SelfieViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SelfieViewController.swift
+//  RunMapGB
+//
+//  Created by emil kurbanov on 07.06.2022.
+//
+
+import Foundation
